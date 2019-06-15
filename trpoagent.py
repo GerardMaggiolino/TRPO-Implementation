569aa3ec82adb4ee2486ed2bb14c9e72444cf7aa
Gerard Maggiolino <gerardmaggiolino@gmail.com>
2019-06-15 08:48:22 -0700

    KL divergence constraint and line search.

--- trpoagent.py
+++ trpoagent.py
@@ -1,24 +1,31 @@
 """
-TODO: File holds self contained TRPO agent with simple interface.
+File holds self contained TRPO agent with simple interface.
 """
 import torch
 import gym
+from copy import deepcopy
+from torch.nn.utils.convert_parameters import parameters_to_vector
+from torch.nn.utils.convert_parameters import vector_to_parameters
 
 
 class TRPOAgent:
-    def __init__(self, policy, optim, discount):
+
+    def __init__(self, policy, optim, discount, delta=0.01):
         self.policy = policy
         self.optim = optim
         self.discount = discount
+        self.delta = delta
 
         policy_modules = [module for module in policy.modules() if not
                           isinstance(module, torch.nn.Sequential)]
         self.action_dims = policy_modules[-1].out_features
         self.distribution = torch.distributions.normal.Normal
-        self.logstd = torch.ones(self.action_dims)
+        self.logstd = torch.ones(self.action_dims, requires_grad=True)
+        with torch.no_grad():
+            self.logstd /= self.logstd.exp()
 
         self.buffers = {'log_prob': [], 'action': [], 'episode_reward': [],
-                        'completed_reward': []}
+                        'completed_reward': [], 'states': []}
 
     def __call__(self, state):
         """
@@ -34,11 +41,14 @@ class TRPOAgent:
             Action choice for each action dimension.
         """
         state = torch.as_tensor(state, dtype=torch.float32)
-        normal_dist = self.distribution(self.policy(state), self.logstd)
+        normal_dist = self.distribution(self.policy(state), self.logstd.exp())
         action = normal_dist.sample()
         self.buffers['action'].append(action)
+        print(action)
+        exit()
         self.buffers['log_prob'].append(normal_dist.log_prob(action))
-        return action
+        self.buffers['states'].append(state)
+        return action.numpy()
 
     def update(self, reward, done):
         self.buffers['episode_reward'].append(reward)
@@ -49,29 +59,80 @@ class TRPOAgent:
             self.buffers['completed_reward'].extend(episode_reward)
             self.buffers['episode_reward'] = []
 
+    def kl(self, new_policy, new_std, states):
+        with torch.no_grad():
+            old_dist = self.distribution(self.policy(states), self.logstd.exp())
+            new_dist = self.distribution(new_policy(states), new_std.exp())
+            kl_matrix = torch.distributions.kl.kl_divergence(old_dist, new_dist)
+
+        return kl_matrix.sum(1).mean()
+
+    def surrogate_objective(self, new_policy, new_std, states, advantages):
+        with torch.no_grad():
+            new_dist = self.distribution(new_policy(states), new_std.exp())
+            new_prob = new_dist.log_prob(torch.stack(self.buffers['action']))
+            ratio = new_prob / torch.stack(self.buffers['log_prob'])
+            return (ratio * advantages.view(-1, 1)).mean()
+
+    def line_search(self):
+        gradients = parameters_to_vector(
+            [param.grad for param in self.policy.parameters()])
+        states = torch.stack(self.buffers['states'])
+        current_parameters = parameters_to_vector(self.policy.parameters())
+        # TODO: Fix the initial step size to be according to paper.
+        step_size = 1
+
+        new_policy = deepcopy(self.policy)
+        vector_to_parameters(current_parameters + step_size * gradients,
+                             new_policy.parameters())
+        new_std = self.logstd.detach() + step_size * self.logstd.grad
+
+        decay = 2
+        line_search_attempts = 10
+        for attempt in range(line_search_attempts):
+
+            if self.kl(new_policy, new_std, states) > self.delta:
+                print('Shrinking Gradients')
+                step_size /= decay
+                vector_to_parameters(current_parameters + step_size * gradients,
+                                     new_policy.parameters())
+                new_std = self.logstd.detach() + step_size * self.logstd.grad
+            else:
+                break
+        else:
+            return self.policy, self.logstd
+
+        return new_policy, new_std.requires_grad_()
+
     def optimize(self):
+        if len(self.buffers['completed_reward']) == 0:
+            return
+
+        # Normalize rewards over episodes
         self.optim.zero_grad()
         rewards = torch.tensor(self.buffers['completed_reward'])
-        returnboi = rewards.sum().item()
+        print(rewards.sum() / rewards.numel())
         rewards = (rewards - rewards.mean()) / rewards.std()
         num_steps_in_batch = rewards.numel()
 
+        # Find REINFORCE gradient
         expected_reward = 0
         for rew, prob in zip(rewards, self.buffers['log_prob']):
-            expected_reward -= prob * rew
+            expected_reward += prob * rew
         expected_reward /= num_steps_in_batch
         expected_reward.mean().backward()
-        self.optim.step()
 
-        del self.buffers['log_prob'][:num_steps_in_batch]
-        del self.buffers['action'][:num_steps_in_batch]
-        self.buffers['completed_reward'] = []
+        # Line search from the initial gradient
+        self.policy, self.logstd = self.line_search()
 
-        return returnboi
+        # Update buffers removing processed steps
+        for key, storage in self.buffers.items():
+            if key != 'episode_reward':
+                del storage[:num_steps_in_batch]
 
 
 def main():
-    env = gym.make('LundarLanderContinuous-v2')
+    env = gym.make('LunarLanderContinuous-v2')
 
     nn = torch.nn.Sequential(torch.nn.Linear(8, 32), torch.nn.Tanh(),
                              torch.nn.Linear(32, 2))
@@ -79,7 +140,7 @@ def main():
     agent = TRPOAgent(nn, opt, 0.99)
 
     iterations = 10
-    steps = 5000
+    steps = 10000
 
     ob = env.reset()
     for _ in range(iterations):
@@ -88,7 +149,7 @@ def main():
             agent.update(rew, done)
             if done:
                 ob = env.reset()
-        print(agent.optimize())
+        agent.optimize()
 
 
 main()
